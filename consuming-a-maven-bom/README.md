04e55a1f1f5fb64c96644fb3b6a0600d51525c69
David Groomes <dwgroomes@gmail.com>
2020-10-02 11:06:47 -0500

    Clean up and showcase strict dependency overriding

--- consuming-a-maven-bom/README.md
+++ consuming-a-maven-bom/README.md
@@ -1,29 +1,27 @@
 # consuming-a-maven-bom
 
-NOT YET FULLY IMPLEMENTED
-
-This sub-project shows how to consume a Maven BOM in a Gradle project.
+This sub-project shows how to consume a Maven BOM in a Gradle project and *strictly* override a BOM dependency.
 
 ---
 
 In particular, this project imports the [Jackson project](https://github.com/FasterXML/jackson) BOM dependency: <https://github.com/FasterXML/jackson-bom>.
 This allows Jackson dependencies defined in the BOM to be declared in the Gradle project *without* a version because
-the version will be determined by the BOM (pretty cool!). Also, in theory, we should be able to specifically override
-versions of any dependency defined in the BOM. Unfortunately, this does [not seem to be possible for the Jackson BOM](https://github.com/gradle/gradle/issues/9160)).
-I 
+the version will be determined by the BOM (pretty cool!). Also, we are able to specifically override versions of any
+dependency defined in the BOM. See the block comments in the `build.gradle.kts` file for details.
 
 While I develop this project, I use the Gradle `dependencies` and `dependencyInsight` tasks to observe the resolved
 versions of dependencies. For example:
 
 ```
 ./gradlew dependencies
+./gradlew dependencies --configuration compileClasspath
 ./gradlew dependencyInsight --dependency jackson-bom
 ./gradlew dependencyInsight --dependency jackson-databind
 ```  
 
 ### Referenced Materials
 
-* Gradle docs: [Importing Maven BOMs](https://docs.gradle.org/current/userguide/platforms.html#sub:bom_import)
+* Gradle docs: [_Importing Maven BOMs_](https://docs.gradle.org/current/userguide/platforms.html#sub:bom_import)
 * Gradle docs: [_Declaring Rich Versions_](https://docs.gradle.org/current/userguide/rich_versions.html#sec:strict-version)
 * Blog post (brilliant post as always, by mrhaki): [_Use bill of materials (BOM) As Dependency Constraints_](https://mrhaki.blogspot.com/2019/04/gradle-goodness-use-bill-of-materials.html)
-* GitHub issue: that says [BOM versions cannot be changed](https://github.com/gradle/gradle/issues/9160)  under any circumstance?
+* GitHub issue: [_Support overriding BOM version properties with ext_](https://github.com/gradle/gradle/issues/9160) ?
--- consuming-a-maven-bom/build.gradle.kts
+++ consuming-a-maven-bom/build.gradle.kts
@@ -8,24 +8,72 @@ repositories {
 }
 
 val jacksonBomVersion = "2.11.2" // releases: https://github.com/FasterXML/jackson/wiki/Jackson-Releases
-val jacksonDatabindVersion = "2.11.1" // let's try to force a specific version of jackson-databind different from what is defined in the BOM
+val jacksonDatabindVersion = "2.11.1" // let's try to force a downgrade to a specific version of jackson-databind which is different from what is defined in the BOM
 
 dependencies {
-    // Can we enforce the specific version (2.11.1) of jackson-databind that we want? Can we use Gradle constraints'
-    // https://docs.gradle.org/current/userguide/dependency_constraints.html#sec:adding-constraints-transitive-deps
-    constraints {
-        implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonDatabindVersion") // this attempt to override doesn't work? the bom version seems to be an unchangeable "constraint"
-    }
-
+    /**
+     * Importing a Maven BOM enables convenience and safety with managing transitive dependencies. In this case, it
+     * let's us omit the versions of Jackson dependency declarations declared later in this 'dependencies' block. That's
+     * nice! Also, the versions of Jackson dependencies that it automatically uses are a set of "known to work well
+     * together" versions. This is a useful safeguard that should reduce our chances of getting an unwelcome NoClassDefFoundError
+     * at runtime.
+     *
+     * Read about Gradle's support for Maven BOMs at https://docs.gradle.org/current/userguide/platforms.html#sub:bom_import
+     */
     implementation(platform("com.fasterxml.jackson:jackson-bom:$jacksonBomVersion"))
+
+    /**
+     * Notice how we don't have to declare a version in these dependency declarations! This is because the earlier Maven
+     * BOM import already defines specific versions for each of these dependencies.
+     */
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     implementation("com.fasterxml.jackson.core:jackson-core")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonDatabindVersion") { // this attempt to override doesn't work! the bom version seems to be an unchangeable "constraint"
-        version {
-            strictly(jacksonDatabindVersion) // this attempt to override the *DOES* work! See https://docs.gradle.org/current/userguide/rich_versions.html#sec:strict-version
-        }
-    }
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
 
+    /**
+     * In this contrived case, we want to override the version of the 'jackson-databind' dependency that is defined in
+     * the Maven BOM. Instead, for whatever reason, we want to downgrade to a lower version of this dependency for
+     * compilation and runtime. We can accomplish that by using the "stricly" rule. Read about this feature at
+     * https://docs.gradle.org/current/userguide/rich_versions.html#sec:strict-version
+     */
+    implementation("com.fasterxml.jackson.core:jackson-databind") {
+        version {
+            strictly(jacksonDatabindVersion)
+        }
+    }
+
+    /**
+     * Finally, when all is said and done, Gradle will resolve our dependencies to the following versions. This is the
+     * output of the command `./gradlew dependencies --configuration compileClasspath`:
+     *
+        > Task :dependencies
+
+        ------------------------------------------------------------
+        Root project
+        ------------------------------------------------------------
+
+        compileClasspath - Compile classpath for source set 'main'.
+        +--- com.fasterxml.jackson:jackson-bom:2.11.2
+        |    +--- com.fasterxml.jackson.core:jackson-annotations:2.11.2 (c)
+        |    +--- com.fasterxml.jackson.core:jackson-core:2.11.2 (c)
+        |    +--- com.fasterxml.jackson.core:jackson-databind:2.11.2 -> 2.11.1 (c)
+        |    +--- com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.11.2 (c)
+        |    \--- com.fasterxml.jackson.module:jackson-module-parameter-names:2.11.2 (c)
+        +--- com.fasterxml.jackson.core:jackson-annotations -> 2.11.2
+        +--- com.fasterxml.jackson.core:jackson-core -> 2.11.2
+        +--- com.fasterxml.jackson.module:jackson-module-parameter-names -> 2.11.2
+        |    +--- com.fasterxml.jackson.core:jackson-core:2.11.2
+        |    \--- com.fasterxml.jackson.core:jackson-databind:2.11.2 -> 2.11.1
+        |         +--- com.fasterxml.jackson.core:jackson-annotations:2.11.1 -> 2.11.2
+        |         \--- com.fasterxml.jackson.core:jackson-core:2.11.1 -> 2.11.2
+        +--- com.fasterxml.jackson.dataformat:jackson-dataformat-csv -> 2.11.2
+        |    +--- com.fasterxml.jackson.core:jackson-databind:2.11.2 -> 2.11.1 (*)
+        |    +--- com.fasterxml.jackson.core:jackson-annotations:2.11.2
+        |    \--- com.fasterxml.jackson.core:jackson-core:2.11.2
+        \--- com.fasterxml.jackson.core:jackson-databind:{strictly 2.11.1} -> 2.11.1 (*)
+
+        (c) - dependency constraint
+        (*) - dependencies omitted (listed previously)
+     */
 }
