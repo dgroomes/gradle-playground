aa87ff08f34bd0b0bc209085cf4f79fa33af2a6f
David Groomes <dwgroomes@gmail.com>
2021-01-23 15:59:00 -0600

    Upgrade to Gradle 6.8.1; define 'assembleAll' task

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+/**
+ * A convenience task to assemble all of the included projects. This is useful as a quick smoke test of the Gradle config
+ * and a general environmental test.
+ */
+tasks.register("assembleAll") {
+    dependsOn(gradle.includedBuild("consuming-a-maven-bom").task(":assemble"))
+    dependsOn(gradle.includedBuild("java-preview-features").task(":assemble"))
+    dependsOn(gradle.includedBuild("java-preview-features-groovy-dsl").task(":assemble"))
+
+    // Skip 'java-toolchain' because it requires Java 16 and I don't know how to get multiple Java's installed in the
+    // GitHub Actions build environment.
+//    dependsOn(gradle.includedBuild("java-toolchain").task(":assemble"))
+
+    dependsOn(gradle.includedBuild("multi-module").task(":module-a:assemble"))
+    dependsOn(gradle.includedBuild("multi-module").task(":module-b:assemble"))
+    dependsOn(gradle.includedBuild("plugin").task(":assemble"))
+}
