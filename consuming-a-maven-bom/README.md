82c594c97023822cc761623b466bebd9eac8bc7c
David Groomes <dwgroomes@gmail.com>
2023-07-29 11:39:52 -0500

    Dependency locking

--- consuming-a-maven-bom/README.md
+++ consuming-a-maven-bom/README.md
@@ -10,17 +10,31 @@ This allows Jackson dependencies defined in the BOM to be declared in the Gradle
 the version will be determined by the BOM (pretty cool!). Also, we are able to specifically override versions of any
 dependency defined in the BOM. See the block comments in the `build.gradle.kts` file for details.
 
+We can make sense of the final dependency tree if we look at the `gradle.lockfile` which is generated when we use
+Gradle's [dependency locking](https://docs.gradle.org/current/userguide/dependency_locking.html) feature.
+
 
 # Instructions
 
-Follow these instructions to build and run the program:
+Follow these instructions to resolve and inspect the dependencies.
 
 1. Use Java 17
 2. Read the `build.gradle.kts` file
    * This file has code comments that describe the core concepts. Read the file in detail.
-3. Make some dependency observations
-   * Use any of the following commands to make observations about the dependencies in the project. These commands will
-     describe resolved and overridden dependency versions.
+3. Trigger dependency resolution and lock the dependencies
+   * ```shell
+     ./gradlew dependencies --write-locks
+     ```
+   * Note: Using the `dependencies` task is arbitrary. Any task that resolves dependencies will work.
+   * Running a dependency-resolving task with `--write-locks` only has an effect on this project when there is a newer
+     version of the Jackson BOM available than there was when this task was last run. The project declares a dynamic
+     version dependency on the Jackson BOM. For example, if the project declares `2.15.+` then if `2.15.2` is the latest
+     version of the Jackson BOM but the project was last built when `2.15.1` was the latest version, then this task will
+     update the `gradle.lockfile` to use `2.15.2`.
+4. Make some dependency observations
+   * Study the `gradle.lock` file. This is a convenient way to get a holistic view of the resolved dependencies in the
+     project. Alternatively, use any of the following commands to inspect the resolved dependencies in the project.
+     These commands are especially helpful in describing when dependency rules override one another.
    * ```shell
      ./gradlew dependencies
      ```
@@ -39,7 +53,7 @@ Follow these instructions to build and run the program:
 
 General clean-ups, changes and things I wish to implement for this project:
 
-* [ ] Consider using dependency locking as way to more concretely show what versions are being used. See the
+* [x] DONE Consider using dependency locking as way to more concretely show what versions are being used. See the
   [Gradle docs on dependency locking](https://docs.gradle.org/current/userguide/dependency_locking.html).
 
 
--- consuming-a-maven-bom/build.gradle.kts
+++ consuming-a-maven-bom/build.gradle.kts
@@ -6,79 +6,86 @@ repositories {
     mavenCentral()
 }
 
-val jacksonBomVersion = "2.15.2" // Jackson releases: https://github.com/FasterXML/jackson/wiki/Jackson-Releases
+val jacksonBomVersion = "2.15.+" // Jackson releases: https://github.com/FasterXML/jackson/wiki/Jackson-Releases
 val jacksonDatabindVersion = "2.15.1" // let's try to force a downgrade to a specific version of jackson-databind which is different from what is defined in the BOM
 
 dependencies {
-    /**
-     * Importing a Maven BOM enables convenience and safety with managing transitive dependencies. In this case, it
-     * lets us omit the versions of Jackson dependency declarations declared later in this 'dependencies' block. That's
-     * nice! Also, the versions of Jackson dependencies that it automatically uses are a set of "known to work well
-     * together" versions. This is a useful safeguard that should reduce our chances of getting an unwelcome NoClassDefFoundError
-     * at runtime.
-     *
-     * Read about Gradle's support for Maven BOMs at https://docs.gradle.org/current/userguide/platforms.html#sub:bom_import
-     */
+    /*
+    Importing a Maven BOM enables convenience and safety with managing transitive dependencies. In this case, it
+    lets us omit the versions of Jackson dependency declarations declared later in this 'dependencies' block. That's
+    nice! Also, the versions of Jackson dependencies that it automatically uses are a set of "known to work well
+    together" versions. This is a useful safeguard that should reduce our chances of getting unwelcome problems like
+    a NoClassDefFoundError exception at runtime.
+
+    Read about Gradle's support for Maven BOMs at https://docs.gradle.org/current/userguide/platforms.html#sub:bom_import
+    */
     implementation(platform("com.fasterxml.jackson:jackson-bom:$jacksonBomVersion"))
 
-    /**
-     * Notice how we don't have to declare a version in these dependency declarations! This is because the earlier Maven
-     * BOM import already defines specific versions for each of these dependencies.
-     */
+    /*
+    Notice how we don't have to declare a version in these dependency declarations! This is because the earlier Maven
+    BOM import already defines specific versions for each of these dependencies.
+    */
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.module:jackson-module-parameter-names")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv")
 
-    /**
-     * In this contrived case, we want to override the version of the 'jackson-databind' dependency that is defined in
-     * the Maven BOM. Instead of using this version, for whatever reason, we want to downgrade to a lower version of
-     * this dependency for compilation and runtime. We can accomplish that by using the "strictly" rule. Read about this
-     * feature at https://docs.gradle.org/current/userguide/rich_versions.html#sec:strict-version
-     */
+    /*
+    In this contrived case, we want to override the version of the 'jackson-databind' dependency that is defined in
+    the Maven BOM. Instead of using this version, for whatever reason, we want to downgrade to a lower version of
+    this dependency for compilation and runtime. We can accomplish that by using the "strictly" rule. Read about this
+    feature at https://docs.gradle.org/current/userguide/rich_versions.html#sec:strict-version
+    */
     implementation("com.fasterxml.jackson.core:jackson-databind") {
         version {
             strictly(jacksonDatabindVersion)
         }
     }
 
-    /**
-     * Finally, when all is said and done, Gradle will resolve our dependencies to the following versions. This is the
-     * output of the command `./gradlew dependencies --configuration compileClasspath`:
-     *
-       > Task :dependencies
+    /*
+     Finally, when all is said and done, Gradle will resolve our dependencies to the following versions. This is the
+     output of the command `./gradlew dependencies --configuration compileClasspath`:
 
-       ------------------------------------------------------------
-       Root project 'consuming-a-maven-bom'
-       ------------------------------------------------------------
+     > Task :dependencies
 
-       compileClasspath - Compile classpath for source set 'main'.
-       +--- com.fasterxml.jackson:jackson-bom:2.15.2
-       |    +--- com.fasterxml.jackson.core:jackson-annotations:2.15.2 (c)
-       |    +--- com.fasterxml.jackson.core:jackson-core:2.15.2 (c)
-       |    +--- com.fasterxml.jackson.core:jackson-databind:2.15.2 -> 2.15.1 (c)
-       |    +--- com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.2 (c)
-       |    \--- com.fasterxml.jackson.module:jackson-module-parameter-names:2.15.2 (c)
-       +--- com.fasterxml.jackson.core:jackson-annotations -> 2.15.2
-       |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
-       +--- com.fasterxml.jackson.core:jackson-core -> 2.15.2
-       |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
-       +--- com.fasterxml.jackson.module:jackson-module-parameter-names -> 2.15.2
-       |    +--- com.fasterxml.jackson.core:jackson-core:2.15.2 (*)
-       |    +--- com.fasterxml.jackson.core:jackson-databind:2.15.2 -> 2.15.1
-       |    |    +--- com.fasterxml.jackson.core:jackson-annotations:2.15.1 -> 2.15.2 (*)
-       |    |    +--- com.fasterxml.jackson.core:jackson-core:2.15.1 -> 2.15.2 (*)
-       |    |    \--- com.fasterxml.jackson:jackson-bom:2.15.1 -> 2.15.2 (*)
-       |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
-       +--- com.fasterxml.jackson.dataformat:jackson-dataformat-csv -> 2.15.2
-       |    +--- com.fasterxml.jackson.core:jackson-databind:2.15.2 -> 2.15.1 (*)
-       |    +--- com.fasterxml.jackson.core:jackson-annotations:2.15.2 (*)
-       |    +--- com.fasterxml.jackson.core:jackson-core:2.15.2 (*)
-       |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
-       \--- com.fasterxml.jackson.core:jackson-databind:{strictly 2.15.1} -> 2.15.1 (*)
+     ------------------------------------------------------------
+     Root project 'consuming-a-maven-bom'
+     ------------------------------------------------------------
 
-       (c) - A dependency constraint, not a dependency. The dependency affected by the constraint occurs elsewhere in the tree.
-       (*) - Indicates repeated occurrences of a transitive dependency subtree. Gradle expands transitive dependency subtrees only once per project; repeat occurrences only display the root of the subtree, followed by this annotation.
+     compileClasspath - Compile classpath for source set 'main'.
+     +--- com.fasterxml.jackson:jackson-bom:2.15.2
+     |    +--- com.fasterxml.jackson.core:jackson-annotations:2.15.2 (c)
+     |    +--- com.fasterxml.jackson.core:jackson-core:2.15.2 (c)
+     |    +--- com.fasterxml.jackson.core:jackson-databind:2.15.2 -> 2.15.1 (c)
+     |    +--- com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.15.2 (c)
+     |    \--- com.fasterxml.jackson.module:jackson-module-parameter-names:2.15.2 (c)
+     +--- com.fasterxml.jackson.core:jackson-annotations -> 2.15.2
+     |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
+     +--- com.fasterxml.jackson.core:jackson-core -> 2.15.2
+     |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
+     +--- com.fasterxml.jackson.module:jackson-module-parameter-names -> 2.15.2
+     |    +--- com.fasterxml.jackson.core:jackson-core:2.15.2 (*)
+     |    +--- com.fasterxml.jackson.core:jackson-databind:2.15.2 -> 2.15.1
+     |    |    +--- com.fasterxml.jackson.core:jackson-annotations:2.15.1 -> 2.15.2 (*)
+     |    |    +--- com.fasterxml.jackson.core:jackson-core:2.15.1 -> 2.15.2 (*)
+     |    |    \--- com.fasterxml.jackson:jackson-bom:2.15.1 -> 2.15.2 (*)
+     |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
+     +--- com.fasterxml.jackson.dataformat:jackson-dataformat-csv -> 2.15.2
+     |    +--- com.fasterxml.jackson.core:jackson-databind:2.15.2 -> 2.15.1 (*)
+     |    +--- com.fasterxml.jackson.core:jackson-annotations:2.15.2 (*)
+     |    +--- com.fasterxml.jackson.core:jackson-core:2.15.2 (*)
+     |    \--- com.fasterxml.jackson:jackson-bom:2.15.2 (*)
+     \--- com.fasterxml.jackson.core:jackson-databind:{strictly 2.15.1} -> 2.15.1 (*)
 
+     (c) - A dependency constraint, not a dependency. The dependency affected by the constraint occurs elsewhere in the tree.
+     (*) - Indicates repeated occurrences of a transitive dependency subtree. Gradle expands transitive dependency subtrees only once per project; repeat occurrences only display the root of the subtree, followed by this annotation.
      */
+}
+
+/*
+As a convenience, we can enable dependency locking so that we can look at the generated `gradle.lockfile` file to see
+the resolved dependencies.
+ */
+dependencyLocking {
+    lockAllConfigurations()
 }
